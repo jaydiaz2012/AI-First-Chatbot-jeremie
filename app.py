171251032d00ddd35044cb7e240ebf45e903eb7e
jdiaz <jayremie.diaz@gmail.com>
2024-11-13 15:40:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if options == "Home" :
 elif options == "About Us" :
      st.title("About Us")
      st.write("# Jeremie Diaz, AI Bot Developer")
-     st.image('images/photo-me.jpg', width=None)
+     st.image('images/photo-me.jpg', use_column_width=True)
      st.write("## Let's connect!")
      st.text("Connect with me via Linkedin : https://www.linkedin.com/in/jandiaz/")
      st.text("Conncet with me via Kaggle: https://www.kaggle.com/jeremiediaz1/")
