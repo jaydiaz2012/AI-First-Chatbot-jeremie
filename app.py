c3b1beee27b794cfb79281d02dbe22dc63d8f0f0
jdiaz <jayremie.diaz@gmail.com>
2024-11-17 21:19:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from openai.embeddings_utils import get_embedding
 import faiss
 import streamlit as st
 import warnings
+from utils.google_map import GoogleMAP
 from utils.routex import Route_X
 from streamlit_option_menu import option_menu
 from streamlit_extras.mention import mention
